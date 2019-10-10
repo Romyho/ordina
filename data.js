e41b6a932b9724bf8a2d11f32a614ddb7ee8b0df
Romy <romy-ho@hotmail.com>
2019-10-10 12:50:32 +0200

    updated

--- data.js
+++ data.js
@@ -1,94 +1,128 @@
-window.onload = function() {
+function json_on_click(){
+  var x = document.getElementById("upload");
+  x.onclick = function() {
+      var files = document.getElementById('fileupload').files;
+    console.log(files);
+    if (files.length <= 0) {
+      return false;
+    }
 
-var data = "https://romyho.github.io/ordina/data/sensorstesttt.json";
+    var fr = new FileReader();
 
-var requests = [d3.json(data), d3.json(data)];
+    fr.onload = function(e) {
+    console.log(e);
+      var result = JSON.parse(e.target.result);
+      var formatted = JSON.stringify(result, null, 2);
+      chart(formatted);
+    }
+    fr.readAsText(files.item(0));
+  };
 
-// load data files
-Promise.all(requests).then(function(responses) {
-
-var Data = responses[0];
-var WheelDiam = 0.7;
-var Circum = WheelDiam * Math.PI;
-var Fact = -Circum/360;
-var Time = [];
-var RotSpeed = [];
-var Speed = [];
-
-for (j in Data){
-  Time.push(Data[j].time.replace(',', '.'))
-  RotSpeed.push(Data[j].rotspeed.replace(',', '.'))
-
-}
-
-for (i in RotSpeed){
-  Speed.push(RotSpeed[i]*Fact)
-}
-var min_speed = 0;
-var max_speed = 1;
-for (i in Speed){
-  if(Speed[i]< min_speed){
-    min_speed = Speed[i];
-  }
-  if(Speed[i]> max_speed){
-    max_speed = Speed[i];
-  }
-}
-
-
-var data1 = [];
-
-for (i in Time){
-  data1.push({
-    x: Time[i]*10,
-    y: Speed[i]
-  });
-}
-
-var sum = 0;
-for (var i = 0; i < Speed.length; i++) {
-  sum += Speed[i]
-}
-
-var avg_speed = sum/Speed.length
-
-var info = d3.selectAll("#info")
-.html(" <FONT SIZE='5'>Training information <br></FONT>" + "<br>"
-+"<strong>Max Speed: </strong><span class='details'>" +  max_speed
-+ "<br></span>" + "<strong> Min Speed: </strong><span class='details'>"
- + min_speed + "<br></span>" +
-  "<strong> AVG Speed: </strong><span class='details'>" +
-   avg_speed + "<br></span>"+ "<strong> Time </strong><span class='details'>"
-    + Time[Time.length-1]+"<strong> sec </strong><span class='details'>" +"<br></span>" )
-
-
-var data = [];
-var dataSeries = { type: "line" };
-
-dataSeries.dataPoints = data1;
-data.push(dataSeries);
-
-//Better to construct options first and then pass it as a parameter
-var options = {
-	zoomEnabled: true,
-	animationEnabled: true,
-	title: {
-		text: "Time Speed Chart"
-	},
-  axisX:{
-  title:"Time (seconds x 10)"
- },
-	axisY: {
-    title: "Speed",
-		includeZero: false,
-		lineThickness: 1
-
-	},
-	data: data
 };
 
-var chart = new CanvasJS.Chart("chartContainer", options);
-chart.render();
+function chart(dataset){
+var Data = JSON.parse(dataset)
 
-})
+  var WheelDiam = 0.7;
+  var Circum = WheelDiam * Math.PI;
+  var Fact = -Circum/360;
+  var Time = [];
+  var RotSpeed = [];
+  var RotSpeed2 = [];
+  var Speed = [];
+  var Speed2 = [];
+  var data1 = [];
+  var data2 = [];
+  var min_speed = 0;
+  var max_speed = 1;
+  var sum = 0;
+  var data = [];
+  var data2_2 = [];
+
+
+  for (j in Data){
+    Time.push(Data[j].time.replace(',', '.'))
+    RotSpeed.push(Data[j].rotspeed.replace(',', '.'))
+
+  }
+
+  for (i in RotSpeed){
+    Speed.push(RotSpeed[i]*Fact)
+    RotSpeed2[i]  = parseInt(RotSpeed[i]+Math.random());
+  }
+
+  for (i in RotSpeed2){
+    Speed2.push(RotSpeed2[i]*Fact)
+  }
+
+  for (i in Speed){
+    if(Speed[i]< min_speed){
+      min_speed = Speed[i];
+    }
+    if(Speed[i]> max_speed){
+      max_speed = Speed[i];
+    }
+  }
+
+  for (i in Time){
+    data1.push({
+      x: Time[i]*10,
+      y: Speed[i]
+    });
+    data2.push({
+      x: Time[i]*10,
+      y: Speed2[i]
+    })
+  }
+
+
+  for (var i = 0; i < Speed.length; i++) {
+    sum += Speed[i]
+  }
+
+  var avg_speed = sum/Speed.length
+  var distance = parseInt(avg_speed*Time[Time.length-1])
+console.log(distance);
+  var info = d3.selectAll("#info")
+  .html(" <FONT SIZE='5'>Training information <br></FONT>" + "<br>"
+  +"<strong>Max Speed:  </strong><span class='details'>" +  max_speed
+  + "<br></span>" + "<strong> Min Speed:  </strong><span class='details'>"
+   + min_speed + "<br></span>" +
+    "<strong> AVG Speed: </strong><span class='details'>" +
+     avg_speed + "<br></span>"+ "<strong> Time:   </strong><span class='details'>"
+      + Time[Time.length-1]+"<strong> sec </strong><span class='details'>" +"<br></span>"+
+       "<strong> Distance:   </strong><span class='details'>"+ distance +"<strong> m </strong><span class='details'>"
+        +"<br></span>" )
+
+
+  var dataSeries = { type: "line" };
+  var dataSeries2 = { type: "line" };
+
+  dataSeries2.dataPoints = data2;
+  dataSeries.dataPoints = data1;
+  data.push(dataSeries);
+  data2_2.push(dataSeries2);
+
+
+  var options = {
+  	zoomEnabled: true,
+  	animationEnabled: true,
+  	title: {
+  		text: "Time Speed Chart"
+  	},
+    axisX:{
+    title:"Time (seconds x 10)"
+   },
+  	axisY: {
+      title: "Speed",
+  		includeZero: false,
+  		lineThickness: 1
+
+  	},
+    data:data
+  };
+
+  var chart = new CanvasJS.Chart("chartContainer",options);
+  chart.render();
+
 }
